d3568d14287836b7588091df555314e934e64f45
PrueDeeDoo <cbianco1301.1@gmail.com>
2024-12-08 10:15:24 -0700

    Update 1.1
    Updated to add a shareable ID input and output!

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const checkbox1 = document.getElementById("c1");
 const checkbox2 = document.getElementById("c2");
 const checkbox3 = document.getElementById("c3");
-const button = document.getElementById("makeList");
+const makeListButton = document.getElementById("makeList");
 const result = document.getElementById("resultDisplay");
+const makeSLB = document.getElementById("makeSharedList");
+const sharedIDIn = document.getElementById("sharedIDInput");
+const outputID = document.getElementById("outputSharedID");
+const sharedIDDisplay = document.getElementById("sharedIDText");
+const copySharedIDButton = document.getElementById("copySharedID");
 
-button.addEventListener("click", function() {
+
+
+makeListButton.addEventListener("click", function() {
   let selectedItems = [];
+  let sharedID = [];
 
   if (checkbox1.checked) {
-    selectedItems.push("Object Name1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Price&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SKU&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Link");
-  }
-  if (checkbox2.checked) {
-    selectedItems.push("Object Name2");
-  }
-  if (checkbox3.checked) {
-    selectedItems.push("Object Name3");
+    sharedID.push("1")
+    selectedItems.push(c1.nextElementSibling.textContent);
+  } else {
+    sharedID.push("0")
   }
 
+  
+  if (checkbox2.checked) {
+    sharedID.push("1")
+    selectedItems.push(c2.nextElementSibling.textContent);
+  } else {
+    sharedID.push("0")
+  }
+
+  
+  if (checkbox3.checked) {
+    sharedID.push("1")
+    selectedItems.push(c3.nextElementSibling.textContent);
+  } else {
+    sharedID.push("0")
+  }
+
+
+  
   let displayText = "";
 
   if (selectedItems.length > 0) {
@@ -26,4 +49,44 @@ button.addEventListener("click", function() {
   }
 
   result.innerHTML = displayText;
+  sharedIDDisplay.innerHTML = "Below is your shared ID:";
+  outputID.innerHTML = sharedID.join("");
+  copySharedIDButton.style.display = "block";
+});
+
+sharedIDIn.addEventListener("input", function() {
+  this.value = this.value.replace(/[^0-1]/g, "");
+});
+
+makeSLB.addEventListener("click", function() {
+  let sharedID = "";
+  const digits = sharedIDIn.value;
+  const listOut = digits.split("").map(Number);
+  //result.innerHTML = String(listOut);
+  let checkboxMulti = 0;
+  for (let i = 0; i < listOut.length; i++) {
+    checkboxMulti += 1;
+    const checkboxId = "c" + (checkboxMulti);
+    console.log(checkboxId);
+    const currentCheckbox = document.getElementById(checkboxId);
+    if (listOut[i] === 0) {
+      currentCheckbox.checked = false;
+    } else if (listOut[i] === 1) {
+        currentCheckbox.checked = true;
+    }
+  };
+  makeListButton.click();
+});
+
+
+
+copySharedIDButton.addEventListener("click", function() {
+  const textToCopy = outputID.textContent;
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      alert("Shared ID copied to clipboard!");
+    })
+    .catch(err => {
+      console.error("Failed to copy: ", err);
+    });
 });
